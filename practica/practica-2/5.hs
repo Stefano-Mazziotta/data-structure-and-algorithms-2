8435c501b08bae99581c62e989b1cbe06f4a9a7a
stefano mazziotta <smazziotta@foodlus.com>
2025-04-25 12:56:44 -0300

    balanced tree

--- practica/practica-2/5.hs
+++ practica/practica-2/5.hs
@@ -4,31 +4,39 @@ la técnica sharing, para que los subárboles sean representados por el mismo á
 siguientes funciones de manera que se puedan compartir la mayor cantidad posible de elementos
 de los árboles creados:
 
-a) completo::a → Int → Tree a, 
+a) completo::a -> Int -> Tree a, 
 tal que dado un valor x de tipo a y un entero d , crea un árbol
 binario completo de altura d con el valor x en cada nodo.
 --}
 
-completo::a -> Int -> Tree a -> Tree a
-data Tree a = E | N (Tree a) a (Tree a)
-
+data Tree a = E | N (Tree a) a (Tree a) deriving Show
+completo :: a -> Int -> Tree a
 completo e 0 = E
-completo e n = N (completo e (n-1) e (completo e (n-1)))
+completo e n = N (completo e (n-1)) e (completo e (n-1))
 
-completo e n =
-    let t = completo e (n-1)
-    in N t e t
+-- completo e n =
+--     let t = completo e (n-1)
+--     in N t e t
+arbolmio = completo 2 1
 
+        -- -- --      1                         1
+        -- -- --     / \
+        -- -- --    /   \       
+        -- -- --   1     1                      2
+        -- --     / \   /  \
+        -- --    1   e e    e                         
 
 {--
-b) balanceado::a → Int → Tree a,
+b) balanceado::a -> Int -> Tree a,
 tal que dado un valor x de tipo a y un entero n, crea un árbol
 binario balanceado de tamaño n, con el valor x en cada nodo.
 --}
 
 balanceado:: a -> Int -> Tree a
+balanceado _ 0 = E
+balanceado x n = 
+        let nLeft = div (n-1) 2 
+            nRight = (n-1) - nLeft
+        in N (balanceado x nLeft) x (balanceado x nRight)
 
--- 7 inclusive
-
--- 8, 9 y 10, ultima teoria
-
+myBalancedTree = balanceado 1 3
